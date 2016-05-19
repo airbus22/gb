efced76dba6664db04ce7d5f80ef78e7b9851820
airbus22 <airbus22@tlen.pl>
2016-05-19 13:23:33 +0200

    dodanie projektu w C#

---
+++ zabawa_klasami/zabawa_klasami/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace zabawa_klasami
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(51, 45);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(43, 13);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "Nazwa:";
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(51, 78);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(32, 13);
+            this.lbl_2.TabIndex = 1;
+            this.lbl_2.Text = "Wiek";
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Location = new System.Drawing.Point(51, 111);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(91, 13);
+            this.lbl_3.TabIndex = 2;
+            this.lbl_3.Text = "Pojemność silnika";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_3;
+    }
+}
+
---
+++ zabawa_klasami/zabawa_klasami/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zabawa_klasami
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //toyota moja = new toyota();
+            toyota moja = new toyota("Auris", 4, 1364);
+            lbl_1.Text = moja.nazwa;
+            lbl_2.Text = moja.wiek.ToString();
+            lbl_3.Text = moja.poj_silnika.ToString();
+        }        
+    }
+
+    class toyota
+    {
+        //public toyota()
+        //{
+        //    this.nazwa = null;
+        //    this.wiek = 0;
+        //    this.poj_silnika = 0;
+        //}
+
+        public toyota(string nazwa, int wiek, int poj_silnika)
+        {
+            this.nazwa = nazwa;
+            this.wiek = wiek;
+            this.poj_silnika = poj_silnika;
+        }
+
+        //public string Nazwa
+        //{
+        //    get { return this.nazwa; }
+        //    set { this.nazwa = value; }
+        //}
+
+        //public int Wiek
+        //{
+        //    get { return this.wiek; }
+        //    set { this.wiek = value; }
+        //}
+
+        //public int Poj_silnika
+        //{
+        //    get { return this.poj_silnika; }
+        //    set { this.poj_silnika = value; }
+        //}
+
+        //string Nazwa;
+        //int Wiek;
+        //int Poj_silnika;
+
+        public string nazwa {get; set; }
+        public int wiek { get; set; }
+        public int poj_silnika { get; set; }
+    }
+}
